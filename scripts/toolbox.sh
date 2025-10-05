8800211bf92d6ecc1670a38ddea106c0674edfbd
Kaj <5012541+bykaj@users.noreply.github.com>
2025-10-05 11:10:23 +0000

    refactor(scripts): remove old scripts in favor of justfile commands

--- scripts/toolbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pod=$(kubectl get pods -n rook-ceph -l app.kubernetes.io/name=rook-ceph-tools -o jsonpath='{.items[0].metadata.name}')
-if [ -n "$pod" ]; then
-    echo "Pod '${pod}' found; starting Rook Ceph Toolbox."
-    kubectl -n rook-ceph exec -it $pod -- bash
-else
-    echo "Cannot start Rook Ceph Toolbox; no pods found."
-fi
